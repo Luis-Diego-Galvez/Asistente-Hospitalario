1c3f7146838bc8df16974642b5c35fc0c3c859f5
Luis-Diego-Galvez <60449586+Luis-Diego-Galvez@users.noreply.github.com>
2022-09-16 10:30:43 -0300

    Modelos 01

--- Models/Especialidad.cs
+++ Models/Especialidad.cs
@@ -9,5 +9,17 @@ namespace Asistente_Hospitalario_de_Pacientes_y_Cirugías.Models
     public class Especialidad
     {
         //ATRIBUTES
+        private string CodigoEspecialidad;
+        private string NombreEspecialidad;
+
+        public Especialidad(string codigoEspecialidad, string nombreEspecialidad)
+        {
+            CodigoEspecialidad = codigoEspecialidad;
+            NombreEspecialidad = nombreEspecialidad;
+        }
+        public Especialidad() { }
+
+        public string Codigo { get => this.CodigoEspecialidad; set => this.CodigoEspecialidad = value;  }
+        public string Nombre { get => this.NombreEspecialidad; set => this.NombreEspecialidad = value;  }
     }
 }
